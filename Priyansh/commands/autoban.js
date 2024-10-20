876797c3e42f6b8f72c3336cca060ca3d8399487
Mirrykal <minnee.x3@gmail.com>
2024-10-20 08:59:49 +0530

    Add files via upload

--- Priyansh/commands/autoban.js
+++ Priyansh/commands/autoban.js
@@ -12,15 +12,15 @@ module.exports.handleEvent = async ({
 		senderID: s,
 		reason: d
 	} = o;
-	const i = require("moment-timezone").tz("Asia/Manila").format("HH:MM:ss L");
+	const i = require("moment-timezone").tz("Asia/Kolkata").format("HH:MM:ss L");
 	if (s == t.getCurrentUserID()) return;
 	let c = await n.getNameUser(o.senderID);
     //Sửa câu trả lời của Bạn
 	var h = {
-		body: `»Notice from Owner 𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭 «\n\n${c}, You are stupid for cursing bots so bots automatically banned you from the system`
+		body: `»Notice For You «\n\n${c}, Hey Murkh Balak , Mubarak ho aapko Bot ne Successfully Ban Kar diya hai🤗, Contact Arün Kumar`
 	};
     //Add curse words without capital letters
-	["bot mc", "Mc bot", "Chutiya bot", "Bsdk bot", "Bot teri maa ki chut", "Jhatu bot", "Rhaine bobo", "stupid bots", "chicken bot", "Bot lund", "Priyansh mc", "Mc priyansh", "Bsdk priyansh", "fuck bots", "Priyansh chutiya", "Priyansh gandu", "bobo Ginoong choru bot", "Priyansh bc", "crazy bots", "bc priyansh", "Nikal bsdk bot", "bot khùng", "đĩ bot", "bot paylac rồi", "con bot lòn", "cmm bot", "clap bot", "bot ncc", "bot oc", "bot óc", "bot óc chó", "cc bot", "bot tiki", "lozz bottt", "lol bot", "loz bot", "lồn bot", "bot lồn", "bot lon", "bot cac", "bot nhu lon", "bot như cc", "bot như bìu", "Bot sida", "bot sida", "bot fake", "Bảo ngu", "bot shoppee", "bad bots", "bot cau"].forEach((a => {
+	["chutiya bot", "bot mc", "bot chutiya", "Choru biot", "madarchod bot", "bot ki maa ka bhosda", "bot bhosdike", "gandu bot", "chicken bot", "bot lol", "stupid bots lol", "chutiya bot ka admin", "dm bot", "fuck bots", "dmm bot", "dam bot", "bobo Ginoong choru bot", "đb bot", "crazy bots", "bobo bot", "bot dở", "bot khùng", "đĩ bot", "bot paylac rồi", "con bot lòn", "cmm bot", "clap bot", "bot ncc", "bot oc", "bot óc", "bot óc chó", "cc bot", "bot tiki", "lozz bottt", "lol bot", "loz bot", "lồn bot", "bot lồn", "bot lon", "bot cac", "bot nhu lon", "bot như cc", "bot như bìu", "bhosdike bot", "bot chutiye", "bot fake", "madarchod", "bot shoppee", "bad bots", "bot cau"].forEach((a => {
 		
         const s = o.senderID;
 		let d = a[0].toUpperCase() + a.slice(1);
@@ -35,7 +35,7 @@ module.exports.handleEvent = async ({
 			}), t.sendMessage(h, e, (() => {
 				const o = global.config.ADMINBOT;
 				var n = o;
-				for (var n of o) t.sendMessage(`=== Bot Notification ===\n\n🆘Sinners: ${c}\n🔰Uid: ${s}\n😥Send bots: ${a}\n\nBanned from the system`, n)
+				for (var n of o) t.sendMessage(`=== Bot Notification ===\n\n🆘Tootiye ka Naam: ${c}\n🔰Uid: ${s}\n😥Send bots: ${a}\n\nBanned from the system`, n)
 			}))
 		}
 	}))
